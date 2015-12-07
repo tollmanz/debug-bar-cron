a7320e50cb102d6a595bbd6f254c10fceab84f62
jrfnl <github_nospam@adviesenzo.nl>
2015-12-07 21:37:16 +0100

    Minor refactoring to reduce code complexity.
    * Adjusted scrutinizer ruleset to prevent having to rename all private properties.
    * Extracted some code snippets to methods.
    * Simplified escaping/typecasting for some integers.

--- class-debug-bar-cron.php
+++ class-debug-bar-cron.php
@@ -132,32 +132,26 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 
 			// Get the time of the next event.
 			$cron_times          = ( is_array( $this->_crons ) ? array_keys( $this->_crons ) : array() );
-			$unix_time_next_cron = $cron_times[0];
+			$unix_time_next_cron = (int) $cron_times[0];
 			$time_next_cron      = date( 'Y-m-d H:i:s', $unix_time_next_cron );
 
 			$human_time_next_cron = human_time_diff( $unix_time_next_cron );
 
 			// Add a class if past current time and doing cron is not running.
-			$times_class = ( time() > $unix_time_next_cron && 'No' === $this->_doing_cron ) ? ' past' : '';
+			$times_class = $this->is_time_in_past( $unix_time_next_cron ) ? ' past' : '';
 
-			if ( ! class_exists( 'Debug_Bar_Pretty_Output' ) ) {
-				require_once plugin_dir_path( __FILE__ ) . 'inc/debug-bar-pretty-output/class-debug-bar-pretty-output.php';
-			}
+			$this->load_debug_bar_pretty_output();
 
-			// Limit recursion depth if possible - method available since DBPO v1.4.
-			if ( method_exists( 'Debug_Bar_Pretty_Output', 'limit_recursion' ) ) {
-				Debug_Bar_Pretty_Output::limit_recursion( 2 );
-			}
-
-			echo '
+			echo  // WPCS: XSS ok.
+			'
 			<div class="debug-bar-cron">
-				<h2><span>', esc_html__( 'Total Events', 'zt-debug-bar-cron' ), ':</span>', intval( $this->_total_crons ), '</h2>
-				<h2><span>', esc_html__( 'Core Events', 'zt-debug-bar-cron' ), ':</span>', intval( $this->_total_core_crons ), '</h2>
-				<h2><span>', esc_html__( 'Custom Events', 'zt-debug-bar-cron' ), ':</span>', intval( $this->_total_user_crons ), '</h2>
+				<h2><span>', esc_html__( 'Total Events', 'zt-debug-bar-cron' ), ':</span>', $this->_total_crons, '</h2>
+				<h2><span>', esc_html__( 'Core Events', 'zt-debug-bar-cron' ), ':</span>', $this->_total_core_crons, '</h2>
+				<h2><span>', esc_html__( 'Custom Events', 'zt-debug-bar-cron' ), ':</span>', $this->_total_user_crons, '</h2>
 				<h2><span>', esc_html__( 'Doing Cron', 'zt-debug-bar-cron' ), ':</span>', esc_html( $this->_doing_cron ), '</h2>
 				<h2 class="times', esc_attr( $times_class ), '"><span>', esc_html__( 'Next Event', 'zt-debug-bar-cron' ), ':</span>
 					', esc_html( $time_next_cron ), '<br />
-					', intval( $unix_time_next_cron ), '<br />
+					', $unix_time_next_cron, '<br />
 					', esc_html( $this->display_past_time( $human_time_next_cron, $unix_time_next_cron ) ), '
 				</h2>
 				<h2><span>', esc_html__( 'Current Time', 'zt-debug-bar-cron' ), ':</span>', esc_html( date( 'H:i:s' ) ), '</h2>
@@ -171,30 +165,17 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 			echo '
 				<h3>', esc_html__( 'Custom Events', 'zt-debug-bar-cron' ), '</h3>';
 
-			if ( ! is_null( $this->_user_crons ) ) {
-				$this->display_events( $this->_user_crons );
-			} else {
-				echo '
-				<p>', esc_html__( 'No Custom Events scheduled.', 'zt-debug-bar-cron' ), '</p>';
-			}
+			$this->display_events( $this->_user_crons, __( 'No Custom Events scheduled.', 'zt-debug-bar-cron' ) );
 
 			echo '
 				<h3>', esc_html__( 'Core Events', 'zt-debug-bar-cron' ), '</h3>';
 
-			if ( ! is_null( $this->_core_crons ) ) {
-				$this->display_events( $this->_core_crons );
-			} else {
-				echo '
-				<p>', esc_html__( 'No Core Events scheduled.', 'zt-debug-bar-cron' ), '</p>';
-			}
+			$this->display_events( $this->_core_crons, __( 'No Core Events scheduled.', 'zt-debug-bar-cron' ) );
 
 			echo '
 			</div>';
 
-			// Unset recursion depth limit if possible - method available since DBPO v1.4.
-			if ( method_exists( 'Debug_Bar_Pretty_Output', 'unset_recursion_limit' ) ) {
-				Debug_Bar_Pretty_Output::unset_recursion_limit();
-			}
+			$this->reset_debug_bar_pretty_output();
 		}
 
 
@@ -204,14 +185,15 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 		 * This function sorts the cron jobs into core crons, and custom crons. It also tallies
 		 * a total count for the crons as this number is otherwise tough to get.
 		 *
-		 * @return array Array of crons.
+		 * @return array|null Array of crons.
 		 */
 		private function get_crons() {
-			if ( ! is_null( $this->_crons ) ) {
+			if ( is_array( $this->_crons ) ) {
 				return $this->_crons;
 			}
 
-			if ( ! $crons = _get_cron_array() ) {
+			$crons = _get_cron_array();
+			if ( ! is_array( $crons ) || array() === $crons  ) {
 				return $this->_crons;
 			}
 
@@ -241,10 +223,10 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 
 					if ( in_array( $hook, $core_cron_hooks, true ) ) {
 						$this->_core_crons[ $time ][ $hook ] = $data;
-						$this->_total_core_crons += count( $data );
+						$this->_total_core_crons            += count( $data );
 					} else {
 						$this->_user_crons[ $time ][ $hook ] = $data;
-						$this->_total_user_crons += count( $data );
+						$this->_total_user_crons            += count( $data );
 					}
 				}
 			}
@@ -256,12 +238,14 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 		/**
 		 * Displays the events in an easy to read table.
 		 *
-		 * @param array $events Array of events.
-		 *
-		 * @return void|string Void on failure; table display of events on success.
+		 * @param array  $events        Array of events.
+		 * @param string $no_events_msg Message to display if there are no events.
 		 */
-		private function display_events( $events ) {
-			if ( is_null( $events ) || empty( $events ) ) {
+		private function display_events( $events, $no_events_msg ) {
+			// Exit early if no events found.
+			if ( ! is_array( $events ) || empty( $events ) ) {
+				echo '
+				<p>', esc_html( $no_events_msg ), '</p>';
 				return;
 			}
 
@@ -284,7 +268,7 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 							<td';
 
 						// Add a class if past current time.
-						if ( time() > $time && 'No' === $this->_doing_cron ) {
+						if ( $this->is_time_in_past( $time ) ) {
 							echo ' class="past"';
 						}
 
@@ -310,12 +294,13 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 						echo '
 							<td>';
 						if ( isset( $info['interval'] ) ) {
+							$interval = (int) $info['interval'];
 							/* TRANSLATORS: %s is number of seconds. */
-							printf( esc_html__( '%ss', 'zt-debug-bar-cron' ) . '<br />', intval( wp_strip_all_tags( $info['interval'] ) ) );
+							printf( esc_html__( '%ss', 'zt-debug-bar-cron' ) . '<br />', $interval ); // WPCS: XSS ok.
 							/* TRANSLATORS: %s is number of minutes. */
-							printf( esc_html__( '%sm', 'zt-debug-bar-cron' ) . '<br />', intval( wp_strip_all_tags( $info['interval'] ) / 60 ) );
+							printf( esc_html__( '%sm', 'zt-debug-bar-cron' ) . '<br />', ( $interval / 60 ) ); // WPCS: XSS ok.
 							/* TRANSLATORS: %s is number of hours. */
-							printf( esc_html__( '%sh', 'zt-debug-bar-cron' ), intval( wp_strip_all_tags( $info['interval'] ) / ( 60 * 60 ) ) );
+							printf( esc_html__( '%sh', 'zt-debug-bar-cron' ), ( $interval / ( 60 * 60 ) ) ); // WPCS: XSS ok.
 						} else {
 							echo esc_html__( 'Single Event', 'zt-debug-bar-cron' );
 						}
@@ -352,13 +337,7 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 			}
 
 			// Ok, we have an argument, let's pretty print it.
-			if ( defined( 'Debug_Bar_Pretty_Output::VERSION' ) ) {
-				echo Debug_Bar_Pretty_Output::get_output( $args, '', true ); // WPCS: XSS ok.
-			} else {
-				// An old version of the pretty output class was loaded.
-				// Real possibility as there are several DB plugins using the pretty print class.
-				Debug_Bar_Pretty_Output::output( $args, '', true );
-			}
+			$this->print_pretty_output( $args );
 		}
 
 
@@ -382,12 +361,14 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 
 			$schedules = wp_get_schedules();
 			foreach ( $schedules as $interval_hook => $data ) {
-				echo '
+				$interval = (int) $data['interval'];
+				echo  // WPCS: XSS ok.
+				'
 						<tr>
 							<td>', esc_html( $interval_hook ), '</td>
-							<td>', intval( wp_strip_all_tags( $data['interval'] ) ), '</td>
-							<td>', intval( wp_strip_all_tags( $data['interval'] ) / 60 ), '</td>
-							<td>', intval( wp_strip_all_tags( $data['interval'] ) / ( 60 * 60 ) ), '</td>
+							<td>', $interval, '</td>
+							<td>', ( $interval / 60 ), '</td>
+							<td>', ( $interval / ( 60 * 60 ) ), '</td>
 							<td>', esc_html( $data['display'] ) . '</td>
 						</tr>';
 			}
@@ -395,6 +376,18 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 			echo '
 					</tbody>
 				</table>';
+		}
+
+
+		/**
+		 * Verify if a given timestamp is in the past or the future.
+		 *
+		 * @param int $time Timestamp.
+		 *
+		 * @return bool True if the time has passed, false otherwise.
+		 */
+		private function is_time_in_past( $time ) {
+			return ( time() > $time && 'No' === $this->_doing_cron );
 		}
 
 
@@ -412,6 +405,49 @@ if ( ! class_exists( 'ZT_Debug_Bar_Cron' ) && class_exists( 'Debug_Bar_Panel' ) 
 				return sprintf( __( '%s ago', 'zt-debug-bar-cron' ), $human_time );
 			} else {
 				return $human_time;
+			}
+		}
+
+
+		/**
+		 * Load the pretty output class & set the recursion limit.
+		 */
+		private function load_debug_bar_pretty_output() {
+			if ( ! class_exists( 'Debug_Bar_Pretty_Output' ) ) {
+				require_once plugin_dir_path( __FILE__ ) . 'inc/debug-bar-pretty-output/class-debug-bar-pretty-output.php';
+			}
+
+			// Limit recursion depth if possible - method available since DBPO v1.4.
+			if ( method_exists( 'Debug_Bar_Pretty_Output', 'limit_recursion' ) ) {
+				Debug_Bar_Pretty_Output::limit_recursion( 2 );
+			}
+		}
+
+
+		/**
+		 * Print any type of variable with colour coding and a variable type indication.
+		 *
+		 * @param mixed $variable The variable to print.
+		 */
+		private function print_pretty_output( $variable ) {
+			if ( defined( 'Debug_Bar_Pretty_Output::VERSION' ) ) {
+				echo Debug_Bar_Pretty_Output::get_output( $variable, '', true ); // WPCS: XSS ok.
+			} else {
+				// An old version of the pretty output class was loaded.
+				// Real possibility as there are several DB plugins using the pretty print class.
+				Debug_Bar_Pretty_Output::output( $variable, '', true );
+			}
+		}
+
+
+		/**
+		 * Unset recursion depth limit for the pretty output class.
+		 *
+		 * @internal Method available since DBPO v1.4.
+		 */
+		private function reset_debug_bar_pretty_output() {
+			if ( method_exists( 'Debug_Bar_Pretty_Output', 'unset_recursion_limit' ) ) {
+				Debug_Bar_Pretty_Output::unset_recursion_limit();
 			}
 		}
 	} // End of class ZT_Debug_Bar_Cron.
