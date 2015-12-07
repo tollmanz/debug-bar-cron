4fe6740fd830b2228db08d89e4b5e69de37e2f9a
jrfnl <github_nospam@adviesenzo.nl>
2015-12-07 02:03:21 +0100

    Prevent file access from outside of WP.

--- class-debug-bar-cron.php
+++ class-debug-bar-cron.php
@@ -1,5 +1,12 @@
 <?php
 
+// Avoid direct calls to this file.
+if ( ! function_exists( 'add_action' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 /**
  * Add a new Debug Bar Panel.
  */
--- debug-bar-cron.php
+++ debug-bar-cron.php
@@ -9,6 +9,13 @@ Author URI: http://github.com/tollmanz
 Depends: Debug Bar
 */
 
+// Avoid direct calls to this file.
+if ( ! function_exists( 'add_action' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 if ( ! function_exists( 'debug_bar_cron_has_parent_plugin' ) ) {
 	/**
 	 * Show admin notice & de-activate if debug-bar plugin not active.
